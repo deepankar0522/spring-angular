7f5bf14a19872e9667f067518de4f99a44d4f3ea
deepankar0522 <deepravi0522@gmail.com>
2017-03-15 03:14:00 +0530

    "module 1"

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
     selector: 'pm-app',
     template: `
     <div><h1>{{pageTitle}}</h1>
-      <div> First Application</div>
+     <pm-products></pm-products>
       </div>
     `
 })
